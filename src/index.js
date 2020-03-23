aabc36b78ae37ca318f909b20e27f07f29beb287
Cesar Carrillo <sistemas.carrillo01@gmail.com>
2020-03-22 18:30:55 -0600

    OMG we can move a piece!
    Apply validations to use coordinates and cells index to know which cell is clicked and where to move it using appendChild.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,39 @@
 const chessboard = document.querySelector(".chessboard");
-const gameRules = [];
+const cells = chessboard.querySelectorAll("td");
+const gameRules = {
+  dimensions: {
+    cells: 22,
+    rows: 12
+  },
+  emptyCells: ["00", "10", "01", "11"]
+};
+let selectedPiece;
 
 chessboard.addEventListener("click", event => {
   const { target } = event;
-  const isIcon = target.tagName === "I";
+  const isPiece = target.tagName === "I";
 
-  if (!isIcon) return;
+  if (isPiece) {
+    const { className, parentElement: cell } = target;
+    const { cellIndex: x, classList, parentElement: row } = cell;
+    const { rowIndex: y } = row;
+    console.log(x, y);
 
-  const { className, parentElement: cell } = target;
-  const { cellIndex: x, parentElement: row } = cell;
-  const { rowIndex: y } = row;
-  console.log(x, y);
+    if (className.includes("pawn")) {
+      classList.toggle("active");
+      selectedPiece = classList.contains("active") ? target : undefined;
+    }
+  } else {
+    if (!selectedPiece) return;
 
-  if (className.includes("pawn")) {
-    target.parentElement.classList.toggle("active");
+    const cell = target;
+    const { cellIndex: x, parentElement: row } = cell;
+    const { rowIndex: y } = row;
+    const coordinates = "" + x + y;
+
+    if (gameRules.emptyCells.includes(coordinates)) return;
+
+    const index = x + y * gameRules.dimensions.cells;
+    cells[index].appendChild(selectedPiece);
   }
 });
